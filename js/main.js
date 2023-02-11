b64ae1653551a4325fb4135140c9e642a17a41df
Dubisek <dubiisek@gmail.com>
2023-02-11 17:24:31 +0100

    solution

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function removeChar(striga){
+
+    let str = striga
+    let x = str.length -1 
+   str = str.slice(1, x)
+    console.log(str);
+
+
+};
+
+
+removeChar('eloquent')
+
+// function removeChar(striga){
+
+//     let str = striga
+//     let x = str.length -1 
+//    str = str.slice(1, x)
+//   return str
+// };
